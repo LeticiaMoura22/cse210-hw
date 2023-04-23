60b84f7ba9f01518cc5f9bf2e38866d7536074da
Leticia.Moura22 <leticiaaraujomouraa@gmail.com>
2023-04-22 21:22:00 -0300

    Completed and working prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Insert your grade percentage:");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+
+        string letter = "";
+
+
+        if (gradePercentage >= 90)
+        {
+            //Console.Write("A");
+            letter= "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            //Console.Write("B");   
+            letter="B";
+        }
+        else if ( gradePercentage >= 70)
+        {
+            //Console.Write("C");
+            letter="C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            //Console.Write("D");
+            letter="C";
+        }
+        else
+        {
+            //Console.Write("F");
+            letter="F";
+
+        };
+
+        Console.WriteLine($"Your final grade is {letter}");
+
+        //to skip a line in c# use \r ( r didn't worked for me) and \n
+
+        if (gradePercentage >= 70)
+        //if (letter =="C"||letter=="B"||letter=="A")
+        {
+            Console.WriteLine("Congrats!! You've passed this course");
+        }
+        else
+        {
+            Console.WriteLine("Not this time!! Try it again i'll suceed");
+        }
     }
 }
