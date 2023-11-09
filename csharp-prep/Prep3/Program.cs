15b2c4035707f382bfcc24a7fee79d73ddecb930
Leticia.Moura22 <leticiaaraujomouraa@gmail.com>
2023-11-09 14:32:59 -0300

    oi

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,28 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+class Program {
+    public static void Main (string[] args) {
+        Random randomGenerator = new Random();
+        int Number = randomGenerator.Next(1,101);
+        int guess =0;
+            while (guess != Number)
+            {
+                Console.WriteLine("What is your guess");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess>Number)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                else if (guess< Number)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                else
+                {
+                    Console.Write("You guess it!");
+                }
+            }
+        
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+
+        Console.WriteLine("What is your guess");
+        int guess = Console.Read();
+
+        while (guess != magicNumber)
+        {
+            if (guess>magicNumber)
+            {
+                Console.WriteLine("Lower");
+
+            }
+            else (guess<magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        Console.Write("You guess it!");
+
+
+
     }
 }
